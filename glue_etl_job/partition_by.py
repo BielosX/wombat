77bdce81d10b61b77dad169cd60caa9af8b91db5
Mateusz Biel <matbiel@gmail.com>
2025-08-07 12:56:13 +0200

    Reformat glue_etl_job py files

--- glue_etl_job/partition_by.py
+++ glue_etl_job/partition_by.py
@@ -5,14 +5,17 @@ from awsglue.job import Job
 from pyspark import SparkContext
 from pyspark.sql.functions import max, col
 
-JOB_NAME = 'JOB_NAME'
-GLUE_DB_NAME = 'glue_db_name'
-GLUE_TABLE_NAME = 'glue_table_name'
-TABLE_FORMAT = 'glue_table_format'
+JOB_NAME = "JOB_NAME"
+GLUE_DB_NAME = "glue_db_name"
+GLUE_TABLE_NAME = "glue_table_name"
+TABLE_FORMAT = "glue_table_format"
 OUT_BUCKET = "output_bucket"
 WRITER_MODE = "writer_mode"
 
-args = getResolvedOptions(sys.argv, [JOB_NAME, GLUE_DB_NAME, GLUE_TABLE_NAME, TABLE_FORMAT, OUT_BUCKET, WRITER_MODE])
+args = getResolvedOptions(
+    sys.argv,
+    [JOB_NAME, GLUE_DB_NAME, GLUE_TABLE_NAME, TABLE_FORMAT, OUT_BUCKET, WRITER_MODE],
+)
 job_name = args[JOB_NAME]
 db_name = args[GLUE_DB_NAME]
 table_name = args[GLUE_TABLE_NAME]
@@ -24,9 +27,18 @@ glue_ctx = GlueContext(spark_ctx)
 job = Job(glue_ctx)
 job.init(job_name, args)
 
-stat_columns = ["basehp", "baseattack", "basedefense", "basespecialattack", "basespecialdefense", "basespeed"]
+stat_columns = [
+    "basehp",
+    "baseattack",
+    "basedefense",
+    "basespecialattack",
+    "basespecialdefense",
+    "basespeed",
+]
 partition_columns = ["type", "generation"]
-frame = glue_ctx.create_dynamic_frame_from_catalog(database=db_name, table_name=table_name)
+frame = glue_ctx.create_dynamic_frame_from_catalog(
+    database=db_name, table_name=table_name
+)
 max_exprs = [max(c).alias(c) for c in stat_columns]
 data_frame = frame.toDF()
 max_values = data_frame.agg(*max_exprs).collect()[0]
@@ -34,7 +46,8 @@ for name in stat_columns:
     max_value = max_values[name]
     data_frame = data_frame.withColumn(f"{name}_norm", col(name) / max_value)
 
-data_frame.write.partitionBy(*partition_columns) \
-    .format(table_format).mode(writer_mode).save(f"s3://{out_bucket}/pokemons/etl_out/{table_format}/")
+data_frame.write.partitionBy(*partition_columns).format(table_format).mode(
+    writer_mode
+).save(f"s3://{out_bucket}/pokemons/etl_out/{table_format}/")
 
 job.commit()
--- glue_etl_job/report.py
+++ glue_etl_job/report.py
@@ -9,8 +9,10 @@ from pyspark.sql.functions import col
 from datetime import datetime
 from io import BytesIO
 
+
 def group_and_count(frame, column):
     return frame.groupBy(column).count().sort(col("count").desc()).toPandas()
+
 
 def write_cell(worksheet, col_index, rows, frame, even_format, odd_format):
     for row_num in range(0, rows):
@@ -21,13 +23,16 @@ def write_cell(worksheet, col_index, rows, frame, even_format, odd_format):
         else:
             worksheet.write(row_index, col_index, cell_value, odd_format)
 
+
 JOB_NAME = "JOB_NAMEJ"
-GLUE_DB_NAME = 'glue_db_name'
-GLUE_TABLE_NAME = 'glue_table_name'
+GLUE_DB_NAME = "glue_db_name"
+GLUE_TABLE_NAME = "glue_table_name"
 OUT_BUCKET = "output_bucket"
 
-client = boto3.client('s3')
-args = getResolvedOptions(sys.argv, [JOB_NAME, GLUE_DB_NAME, GLUE_TABLE_NAME, OUT_BUCKET])
+client = boto3.client("s3")
+args = getResolvedOptions(
+    sys.argv, [JOB_NAME, GLUE_DB_NAME, GLUE_TABLE_NAME, OUT_BUCKET]
+)
 job_name = args[JOB_NAME]
 db_name = args[GLUE_DB_NAME]
 table_name = args[GLUE_TABLE_NAME]
@@ -38,7 +43,9 @@ glue_ctx = GlueContext(spark_ctx)
 job = Job(glue_ctx)
 job.init(job_name, args)
 
-dynamic_frame = glue_ctx.create_dynamic_frame_from_catalog(database=db_name, table_name=table_name)
+dynamic_frame = glue_ctx.create_dynamic_frame_from_catalog(
+    database=db_name, table_name=table_name
+)
 frame = dynamic_frame.toDF()
 
 by_type = group_and_count(frame, "type")
@@ -50,31 +57,33 @@ by_type_columns = len(by_type.columns)
 by_generation_columns = len(by_generation.columns)
 with pandas.ExcelWriter(buffer, engine="xlsxwriter") as writer:
     by_type.to_excel(writer, sheet_name="Report", startcol=0, index=False, header=True)
-    start_col = by_type_columns+1
-    by_generation.to_excel(writer, sheet_name="Report", startcol=start_col, index=False, header=True)
+    start_col = by_type_columns + 1
+    by_generation.to_excel(
+        writer, sheet_name="Report", startcol=start_col, index=False, header=True
+    )
 
-    workbook  = writer.book
+    workbook = writer.book
     worksheet = writer.sheets["Report"]
 
-    header_format = workbook.add_format({
-        'bold': True,
-        'font_color': 'white',
-        'bg_color': '#4F81BD',
-        'align': 'center'
-    })
+    header_format = workbook.add_format(
+        {"bold": True, "font_color": "white", "bg_color": "#4F81BD", "align": "center"}
+    )
 
-    data_commons = {
-        'align': 'center',
-        'valign': 'vcenter'
-    }
+    data_commons = {"align": "center", "valign": "vcenter"}
 
-    even_cell = workbook.add_format({
-        'bg_color': "#8BB7ED",
-    } | data_commons)
+    even_cell = workbook.add_format(
+        {
+            "bg_color": "#8BB7ED",
+        }
+        | data_commons
+    )
 
-    odd_cell = workbook.add_format({
-        'bg_color': "#BAD0EC",
-    } | data_commons)
+    odd_cell = workbook.add_format(
+        {
+            "bg_color": "#BAD0EC",
+        }
+        | data_commons
+    )
 
     rows = len(by_type.index)
     max_lens = by_type.astype(str).applymap(len).max()
@@ -87,7 +96,7 @@ with pandas.ExcelWriter(buffer, engine="xlsxwriter") as writer:
     rows = len(by_generation.index)
     max_lens = by_generation.astype(str).applymap(len).max()
     for col_num, value in enumerate(by_generation.columns):
-        col_index = start_col+col_num
+        col_index = start_col + col_num
         column_len = max(max_lens[col_num], len(str(value))) + 4
         worksheet.write(0, col_index, value, header_format)
         write_cell(worksheet, col_index, rows, by_generation, even_cell, odd_cell)
